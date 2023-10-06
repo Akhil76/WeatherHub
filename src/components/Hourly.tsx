cb2af704875585e85a2323c1bee3cf08684485dc
AKHIL <akhilpaulweb@gmail.com>
2023-10-06 20:25:34 +0600

    solving scrolling.

---
+++ src/components/Hourly.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ApiKey } from '../OpenApiKey';
+
+
+interface Props {
+    Title: string;
+    dateData: string;
+    loc:string|null;
+}
+function Hourly({ Title,loc, dateData }: Props) {
+   
+    var [data, setData] = useState(Object);
+    useEffect(() => {
+        // Retrieve data from AsyncStorage
+       
+        // Weather api call and data fetch
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=${ApiKey}&q=${loc}&days=1&aqi=no&alerts=no`)
+            .then(res => res.json())
+            .then(res => setData(res))
+            //.then(res => console.log(res))
+            .catch(err => console.log(err))
+
+    },);
+
+
+
+    return (
+        <View style={styles.forecast_area}>
+            <Text style={{ margin: 10 }}>{Title}</Text>
+            <View style={styles.daily_forecast}>
+                {data && data.forecast && data.forecast.forecastday ? (
+                    data.forecast.forecastday[0].hour.map((item: any) => {
+                        const ep_date = new Date(Number(item.time_epoch));
+                        const Time = ep_date.toLocaleString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
+                        return (
+                            <View style={{
+                                margin: 10,
+                                width: "25%",
+                            }}
+                                key={item.time}
+                            >
+                                <Text>{Time}</Text>
+                                <Text>{item.time}</Text>
+                                <Image
+                                    style={styles.tinyLogo}
+                                    source={{
+                                        uri: 'https:' + item.condition.icon
+                                    }}
+                                />
+                                <Text>{item.condition.text}</Text>
+                                <Text>{item.temp_c}°</Text>
+
+                            </View>
+                        )
+                    })
+                ) : (
+                    <Text>Loading...</Text>
+                )}
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+
+    forecast_area: {
+        backgroundColor: "#e6ffcc",
+        margin: 10,
+        borderRadius: 13
+    },
+    daily_forecast: {
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "row",
+
+    },
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
+
+});
+export default Hourly;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,10 +4,11 @@ import { ApiKey } from '../OpenApiKey';
 import { useRoute } from '@react-navigation/native';
 import IconButton from '../components/IconButton';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Hourly from '../components/Hourly';
 
 
 function Home({ navigation }: { navigation: any }) {
-    const city = useRoute()
+
     var [data, setData] = useState(Object);
     const [location, setLocation] = useState<string | null>(null);
 
@@ -91,26 +92,82 @@ function Home({ navigation }: { navigation: any }) {
                             </View>
                         )}
                 </View>
-                <View style={styles.daily_forecast}>
-                    {data.forecast && data.forecast.forecastday.map((day: { date: string; day: { maxtemp_c: number; mintemp_c: number, condition: { text: string, icon: string } }; }) => (
-                        <View style={{
-                            margin: 10,
-                            width: "25%",
-                        }}
-                            key={day.date}
-                        >
-                            <Text>Date: {day.date}</Text>
-                            <Text>{day.day.maxtemp_c}/{day.day.mintemp_c}°C</Text>
-                            <Image
-                                style={styles.tinyLogo}
-                                source={{
-                                    uri: 'https:' + day.day.condition.icon
+                <View style={styles.forecast_area}>
+                    <Text style={{ margin: 10 }}>Daily forcast</Text>
+                    <View style={styles.daily_forecast}>
+
+                        {data.forecast && data.forecast.forecastday.map((day: { date: string; astro: { sunrise: string; sunset: string }; day: { maxtemp_c: string; mintemp_c: string, condition: { text: string, icon: string } }; }) => {
+
+                            let date = new Date(day.date);
+                            let options: Intl.DateTimeFormatOptions = { weekday: 'long' };
+                            let dayname = date.toLocaleDateString('en-US', options);
+                            let max_t = parseInt(day.day.maxtemp_c)
+                            let min_t = parseInt(day.day.mintemp_c)
+                            return (
+                                <View style={{
+                                    width: "30%",
+                                    backgroundColor:"#7CFC00",
+                                    margin: 5,
+                                    padding:5,
+                                    borderRadius:10,
                                 }}
-                            />
-                            <Text>{day.day.condition.text}</Text>
-                            {/* Add more forecast data as needed */}
-                        </View>
-                    ))}
+                                    key={day.date}
+                                >
+                                    <Text>{dayname}</Text>
+                                    <Image
+                                        style={styles.tinyLogo}
+                                        source={{
+                                            uri: 'https:' + day.day.condition.icon
+                                        }}
+                                    />
+                                    <Text>{day.day.condition.text}</Text>
+                                    <Text>{max_t}°/{min_t}°C</Text>
+                                    <Text>Sunrise:{day.astro.sunrise}</Text>
+                                    <Text>Sunset:{day.astro.sunset}</Text>
+                                    {/* Add more forecast data as needed */}
+                                </View>
+                            )
+                        })}
+                    </View>
+                </View>
+                <View style={styles.forecast_area}>
+                    <Text style={{ margin: 10 }}>Hourly Forecast</Text>
+                    <ScrollView horizontal={true}>
+                        {data && data.forecast && data.forecast.forecastday ? (
+                            data.forecast.forecastday[0].hour.map((item: any) => {
+
+
+                                const date = new Date(item.time_epoch * 1000);
+                                const hour = date.getHours();
+                                const minute =  date.getMinutes();
+                                return (
+                                    <View style={{
+                                        flex:1,
+                                        backgroundColor:"#7CFC00",
+                                        margin: 3,
+                                        padding:5,
+                                        borderRadius:10,
+                                        width:60,
+                                    }}
+                                        key={item.time}
+                                    >
+                                       
+                                        <Text>{hour}:{minute}</Text>
+                                        <Image
+                                            style={styles.tinyLogo}
+                                            source={{
+                                                uri: 'https:' + item.condition.icon
+                                            }}
+                                        />
+                                        <Text>{item.chance_of_rain}%</Text>
+                                        <Text>{item.temp_c}°c</Text>
+                                    </View>
+                                )
+                            })
+                        ) : (
+                            <Text>Loading...</Text>
+                        )}
+                    </ScrollView>
                 </View>
             </View>
         </ScrollView>
@@ -137,13 +194,17 @@ const styles = StyleSheet.create({
         padding: 15,
         borderRadius: 13
     },
-    daily_forecast: {
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "row",
+    forecast_area: {
         backgroundColor: "#e6ffcc",
         margin: 10,
         borderRadius: 13
+    },
+    daily_forecast: {
+        display: "flex",
+        justifyContent:"flex-start",
+      
+        flexDirection: "row",
+
     },
     input: {
         height: 40,
@@ -156,8 +217,8 @@ const styles = StyleSheet.create({
         height: 170,
     },
     tinyLogo: {
-        width: 50,
-        height: 50,
+        width: 40,
+        height: 40,
     },
 });
 
